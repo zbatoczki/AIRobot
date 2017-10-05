c59765498a05c656a5de6102ab475fef840229d5
Phillip Porter <phillip.porter54936@outlook.com>
2017-10-05 10:30:38 -0700

    Update double_wheelR3.ino

--- double_wheelR3/double_wheelR3.ino
+++ double_wheelR3/double_wheelR3.ino
@@ -4,7 +4,8 @@
 
 #include <Servo.h>
 // #define ROTATION 663 // According to manual
-#define ROTATION 398  // According to tests?
+#define ROTATION_1 398
+#define ROTATION_2 398
 
 // encoder variables:
 
@@ -31,7 +32,7 @@ volatile int m1 = LOW;
 int loopCnt=0;
 int Ts = 1; //1ms
 
-Servo ST1, ST2; // We'll name the Sabertooth servo channel objects ST1 and ST2.
+Servo ST0, ST1; // We'll name the Sabertooth servo channel objects ST0 and ST1.
                 // For how to configure the Sabertooth, see the DIP Switch Wizard for
                 //   http://www.dimensionengineering.com/datasheets/SabertoothDIPWizard/start.htm
                 // Be sure to select RC Microcontroller Mode for use with this sample.
@@ -52,8 +53,8 @@ Servo ST1, ST2; // We'll name the Sabertooth servo channel objects ST1 and ST2.
 // With all three arguments, we can use 0 to 180 degrees, with 90 being stopped.
 void setup()
 {
-  ST1.attach( 9, 1000, 2000);
-  ST2.attach(10, 1000, 2000);
+  ST0.attach(9, 1000, 2000);
+  ST1.attach(10, 1000, 2000);
 
   // encoder0 setup
   pinMode (encoder0PinA,INPUT); 
@@ -79,8 +80,8 @@ void loop()
     // proportional feedback gain:
     int kp=10;
         
-    float turns0    = ((float)encoder0Pos)/ROTATION;
-    float turns1    = ((float)encoder1Pos)/ROTATION;
+    float turns0    = ((float)encoder0Pos)/ROTATION_1;
+    float turns1    = ((float)encoder1Pos)/ROTATION_2;
     
     Serial.print(" T0: ");
     Serial.print(turns0, DEC);
@@ -96,44 +97,44 @@ void loop()
     error1=kp*(desTurns1-turns1); 
 
     // Control of Motor 0
-    if (error0 < -.05)
+    if (error0 < -.2)
     {
-      power0=75;
+      power0=60;
       //power0=map(error0,-10*kp, 10*kp, 10,80);
     }
-    else if (error0 < .05)
+    else if (error0 < .2)
     {
       power0 = 90;
     }
     else
     {
-      power0=115;
+      power0=120;
       //power0=map(error0,-10*kp, 10*kp, 175,90);
     }
        
     // Control Calc for Motor 1
-    if (error1 < -.05)
+    if (error1 < -.2)
     {
-      power1 = 45;
+      power1 = 60;
       //power1=map(error1,-10*kp, 10*kp, 100,120);
     }
-    else if (error1 < .05)
+    else if (error1 < .2)
     {
       power1 = 90;
     }
     else
     {
-      power1=145;
+      power1=120;
       //power1=map(error1,-10*kp, 10*kp, 80,50);
     }
 
     // sending the control with PWM 
-    //ST1.write(90);
-    //ST2.write(90);
+    ST0.write(90);
+    // ST1.write(90);
+    // ST0.write(power0);
     ST1.write(power1);
-    ST2.write(power0);
     
-    delay(Ts*10);
+    delay(Ts);
 
   encoder0PinALast = n;
   valNew = encoder0Pos;
@@ -196,4 +197,3 @@ void StateB1()
 {
   m1 = digitalRead(encoder1PinB);
 }
-
